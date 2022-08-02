f62486588a3c26feb6cb5ea97ac8d14f21ef3cbd
hyc17986 <huangyouchang@noahgroup.com>
2022-08-02 11:19:01 +0800

    1

--- buildSrc/src/main/groovy/org/devio/as/proj/plugin/TinyPngPlugin.groovy
+++ buildSrc/src/main/groovy/org/devio/as/proj/plugin/TinyPngPlugin.groovy
@@ -1,6 +1,6 @@
 package org.devio.as.proj.plugin
 
-
+import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
@@ -9,14 +9,13 @@ class TinyPngPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        if (!project.plugins.hasPlugin("com.android.application")) {
-            throw new ProjectConfigurationException("plugin:com.android.application must be apply", null)
 
+        if(!project.plugins.hasPlugin("com.android.application")){
+            throw  new ProjectConfigurationException("plugin:com.android.application must apply",null)
         }
-        project.android.registerTransform(new TinyPngPTransform(project))
 
-        //def android = project.extensions.findByType(AppExtension.class)
-        //android.registerTransform(new TinyPngPTransform(project))
-
+        project.android.registerTransform(new TinyPngTransform(project))
+//        def  android = project.extensions.findByType(AppExtension.class)
+//        android.registerTransform(new TinyPngTransform(project))
     }
 }
