357fd31a093e43e7bae6e9db38f9abbcb793b4f4
hyc17986 <huangyouchang@noahgroup.com>
2022-10-14 13:43:44 +0800

    恢复 AspectJ

--- app/src/main/java/com/example/optimize/aspectj/ActivityAspect.kt
+++ app/src/main/java/com/example/optimize/aspectj/ActivityAspect.kt
@@ -8,28 +8,28 @@ import org.aspectj.lang.annotation.Aspect
 
 @Aspect
 class ActivityAspect {
-//    @Around("execution(* androidx.appcompat.app.AppCompatActivity.setContentView(..))")
-//    fun setContentView(joinPoint: ProceedingJoinPoint) {//如果是 before 或者after类型，JoinPoint类型
-//        AdviceCode(joinPoint)
-//    }
-//
-//
-//    //所有标注了 MethodTrace 注解的方法
-//    @Around("execution(@com.example.optimize.aspectj.MethodTrace * *(..))")
-//    fun  methodTrace(joinPoint: ProceedingJoinPoint){
-//        AdviceCode(joinPoint)
-//    }
-//
-//    private fun AdviceCode(joinPoint: ProceedingJoinPoint) {
-//        val signature = joinPoint.signature
-//        val className = signature.declaringType.simpleName
-//        val methodName = signature.name
-//
-//        val time = System.currentTimeMillis()
-//        joinPoint.proceed()
-//        Log.e(
-//            "ActivityAspect",
-//            className + ":" + methodName + "  cost=" + (System.currentTimeMillis() - time)
-//        )
-//    }
+    @Around("execution(* androidx.appcompat.app.AppCompatActivity.setContentView(..))")
+    fun setContentView(joinPoint: ProceedingJoinPoint) {//如果是 before 或者after类型，JoinPoint类型
+        AdviceCode(joinPoint)
+    }
+
+
+    //所有标注了 MethodTrace 注解的方法
+    @Around("execution(@com.example.optimize.aspectj.MethodTrace * *(..))")
+    fun  methodTrace(joinPoint: ProceedingJoinPoint){
+        AdviceCode(joinPoint)
+    }
+
+    private fun AdviceCode(joinPoint: ProceedingJoinPoint) {
+        val signature = joinPoint.signature
+        val className = signature.declaringType.simpleName
+        val methodName = signature.name
+
+        val time = System.currentTimeMillis()
+        joinPoint.proceed()
+        Log.e(
+            "ActivityAspect",
+            className + ":" + methodName + "  cost=" + (System.currentTimeMillis() - time)
+        )
+    }
 }
